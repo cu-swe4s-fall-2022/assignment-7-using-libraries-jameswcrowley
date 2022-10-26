850438d6b4509e0f6e5fd8d61d35c2ca33ad7b9b
James Crowley <62038127+jameswcrowley@users.noreply.github.com>
2022-10-26 16:48:14 -0600

    Added more functionality which now returns a random matrix. Unit tests now pass. Pushing this branch and merging.

--- data_processor.py
+++ data_processor.py
@@ -1,10 +1,8 @@
 # remember to import your libraries!
 import numpy as np
-import numpy.random as random
-
 
 def get_random_matrix(num_rows, num_columns):
-	return np.zeros((num_rows, num_columns))
+	return np.random.rand(num_rows, num_columns)
 
 def get_file_dimensions(file_name):
 	return (0,0)
